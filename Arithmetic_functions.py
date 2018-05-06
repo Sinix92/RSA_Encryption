5d73bb29d984cf393c8bbc6d9b21d0935cf02c9f
Sinix <js.kervella@laposte.net>
2018-05-06 18:35:45 +0200

    Update repo

---
+++ Arithmetic_functions.py
@@ -0,0 +1,147 @@
+import collections
+import matplotlib.pyplot as plt
+
+"""
+Fundamental Theorem of Arithmetic:
+Every positive integer (except the number 1) can be represented in exactly one way apart from rearrangement as a product
+of one or more primes.
+"""
+
+
+# Prime Number Function
+def is_prime_number(number):
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+# Find Prime Factors of a given number
+def find_prime_factors(number):
+    if not is_prime_number(number):
+        set_prime_numbers = [i for i in range(2, (number // 2) + 1) if is_prime_number(i)]
+
+        set_prime_factors = []
+        for prime_number in set_prime_numbers:
+            if number % prime_number == 0:
+                set_prime_factors.append(prime_number)
+
+    else:
+        set_prime_factors = [number]
+
+    return set_prime_factors
+
+
+# Find the Prime Factorization of a given number
+def find_factorization(number, set_prime_factors):
+    set_factorization = []
+    for prime_factor in set_prime_factors:
+        power = -1
+        while number % prime_factor == 0:
+            power += 1
+            number = number / prime_factor
+        set_factorization.append((prime_factor, prime_factor ** power))
+
+    return set_factorization
+
+
+""" 
+Discrete Logarithm Problem:
+Given 12, find the exponent 3 needs to be raised to: 3 ** x % 17 = 12?
+The function just below helps in retrieving the different solutions to this equation.
+
+Diffie-Hellman Key Exchange (used in TLS/SSL): 
+https://www.khanacademy.org/computing/computer-science/cryptography/modern-crypt/v/diffie-hellman-key-exchange-part-2
+"""
+
+
+def find_remainders_prime_modulo(generator, prime_modulus, max_power):
+    list_remainders = []
+    dict_remainders = {}
+    for power in range(1, max_power + 1):
+        remainder = generator ** power % prime_modulus
+        list_remainders.append(remainder)
+
+        if remainder not in dict_remainders:
+            dict_remainders[remainder] = [power]
+        else:
+            dict_remainders[remainder].append(power)
+
+    return collections.Counter(list_remainders), dict_remainders
+
+
+# Phi function (Euler's Totient/ Indicatrice d'Euler)
+def phi_function(number):
+    if number == 1:
+        return 1
+    elif is_prime_number(number):
+        return number - 1
+    else:
+        factors_list = []
+        # no_factors_list = []
+        nb_of_no_factor = 0
+
+        for n in range(1, number):
+            if n != 1 and number % n == 0:
+                factors_list.append(n)
+            else:
+                for factor in factors_list:
+                    if n % factor == 0:
+                        break
+                else:
+                    # no_factors_list.append(n)
+                    nb_of_no_factor += 1
+
+        # return no_factors_list
+        return nb_of_no_factor
+
+
+def share_common_factor(nb1, nb2):
+    nb1, nb2 = sorted([nb1, nb2])
+
+    if nb2 % nb1 == 0:
+        return True
+    else:
+        for nb in range(2, (nb1 // 2) + 1):
+            if nb1 % nb == 0 and nb2 % nb == 0:
+                return True
+
+        return False
+
+
+def test_prime_factorization():
+    my_number = 437231
+    print(find_factorization(my_number, find_prime_factors(my_number)))
+
+
+def test_find_remainders_prime_modulo():
+    my_generator = 3
+    my_prime_modulus = 17
+    my_multiplier = 1
+    my_list_of_remainders, my_dict_remainders = find_remainders_prime_modulo(my_generator, my_prime_modulus,
+                                                                             my_prime_modulus * my_multiplier)
+    print(my_list_of_remainders)
+    print(my_dict_remainders)
+
+
+def test_phi_function():
+    # my_number = 15
+    # print(phi_function(my_number))
+
+    y_coordinates = []
+    for n in range(1, 1001):
+        y_coordinates.append(phi_function(n))
+
+    plt.scatter(range(1, 1001), y_coordinates, s=1)
+    plt.xlabel('n')
+    plt.ylabel('Phi(n)')
+    plt.ylim([0, 1000])
+    plt.xlim([0, 1000])
+    plt.show()
+
+
+if __name__ == '__main__':
+    # test_prime_factorization()
+    # test_find_remainders_prime_modulo()
+    # test_phi_function()
+    share_common_factor(100, 15)
---
+++ RSA_Encryption.py
@@ -0,0 +1,236 @@
+import random
+
+from Arithmetic_functions import is_prime_number, share_common_factor
+from customized_decorators import singleton
+
+
+class Person:
+    PEOPLE = set()
+
+    def __init__(self, name, nb_figures_prime_numbers, start_e_nb, end_e_nb, step_e_nb, start_k_nb, end_k_nb):
+        self.name = name
+        self.e_encrypt_public_key, self.n_encrypt_public_key, self.__d_decrypt_private_key = \
+            RSAMethods.initialize_keys(nb_figures_prime_numbers, start_e_nb, end_e_nb, step_e_nb, start_k_nb, end_k_nb)
+        self.packages = []
+        self.received_packages = []
+        self.dict_person_key = {}
+        Person.PEOPLE.add(self)
+
+    @classmethod
+    def check_person(cls, person):
+        return True if person in cls.PEOPLE else False
+
+    def ask_connection(self, recipients):
+        my_recipients = []
+
+        for recipient in recipients:
+            if recipient not in self.dict_person_key:
+                self.create_package("connection_request", recipients=recipient)
+                my_recipients.append(recipient)
+
+        if my_recipients:
+            self.send_packages()
+            for recipient in my_recipients:
+                recipient.read_packages()
+
+            self.read_packages()
+
+    def provide_connection(self):
+        self.create_package("public_key")
+        self.send_packages()
+
+    def create_package(self, package_type, message=None, recipients=None):
+        if package_type == "public_key":
+            self.packages.append(Package(package_type, (self.e_encrypt_public_key, self.n_encrypt_public_key), self))
+        elif package_type == "connection_request":
+            self.packages.append(Package(package_type, "N/A", self, recipients))
+        elif package_type == "message" and message and recipients:
+            for recipient in recipients:
+
+                if Person.check_person(recipient):
+                    if recipient in self.dict_person_key:
+                        e_encryption_public_key = self.dict_person_key[recipient][0]
+                        n_encryption_public_key = self.dict_person_key[recipient][1]
+                        self.packages.append(Package(package_type,
+                                                     RSAMethods.encrypt(message, e_encryption_public_key,
+                                                                        n_encryption_public_key)
+                                                     , self, recipient))
+                    else:
+                        # ask for public keys
+                        self.ask_connection([recipient])
+                        self.create_package(package_type, message, [recipient])
+
+    def send_packages(self):
+        for package in self.packages:
+            if package.recipient == "All":
+                for person in Person.PEOPLE - {self}:
+                    person.receive_package(package)
+                    print("Message sent from: {} to: {} Description: {}".format(self.name, person.name, package.header))
+            else:
+                package.recipient.receive_package(package)
+                print("Message sent from: {} to: {} Description: {}".format(self.name, package.recipient.name,
+                                                                            package.header))
+
+        del self.packages[:]
+
+    def receive_package(self, package):
+        self.received_packages.append(package)
+
+    def read_packages(self):
+        for package in self.received_packages:
+            if package.header == "public_key":
+                if package.sender not in self.dict_person_key:
+                    self.dict_person_key[package.sender] = package.content
+                    print("{} is speaking: Public Keys coming from: {} Public Key: {}".format(self.name,
+                                                                                              package.sender.name,
+                                                                                              package.content))
+            elif package.header == "connection_request":
+                print("{} is speaking: Connection Request coming from: {}".format(self.name, package.sender.name))
+                self.provide_connection()
+            elif package.header == "message":
+                decrypted_message = RSAMethods.decrypt(package.content, self.__d_decrypt_private_key,
+                                                       self.n_encrypt_public_key)
+                print("{} is speaking: Message coming from: {} Content: {}".format(self.name, package.sender.name,
+                                                                                   decrypted_message))
+
+        del self.received_packages[:]
+
+
+class Package:
+
+    def __init__(self, header, content, sender, recipient="All"):
+        self.header = header
+        self.content = content
+        self.sender = sender
+        self.recipient = recipient
+
+
+class CharToAsciiCode:
+
+    @staticmethod
+    def encrypt(message):
+
+        '''
+        :param message: string of characters
+        :return: list of ascii codes
+        '''
+
+        list_ascii_codes = []
+        for letter in message:  # each character in the message is turned into an ASCII Code
+            list_ascii_codes.append(ord(letter))
+
+        return list_ascii_codes
+
+    @staticmethod
+    def decrypt(list_ascii_codes):
+
+        '''
+        :param list_ascii_codes: list of ascii codes
+        :return: string of characters
+        '''
+
+        decrypted_msg_list = []
+        for decrypted_code in list_ascii_codes:
+            letter = chr(decrypted_code)  # each ASCII Code is turned into a character
+            decrypted_msg_list.append(letter)
+
+        return ''.join(decrypted_msg_list)
+
+
+class RSAMethods:
+
+    @staticmethod
+    def initialize_keys(nb_figures_prime_numbers, start_e_nb, end_e_nb, step_e_nb, start_k_nb, end_k_nb):
+        level_of_dozen = 10 ** (nb_figures_prime_numbers - 1)
+        list_prime_numbers = PrimeNumbers(level_of_dozen, (10 * level_of_dozen) - 1).list
+
+        list_k_numbers = []
+        while not list_k_numbers:  # sometimes list_k_numbers can be empty, hence the loop here
+            p1 = random.choice(list_prime_numbers)
+            list_prime_numbers.remove(p1)  # p2 must be different from p1, so we remove p1 from the list
+            p2 = random.choice(list_prime_numbers)
+            n = p1 * p2
+            phi_n = (p1 - 1) * (p2 - 1)
+
+            list_e_numbers = ENumbers(start_e_nb, end_e_nb, step_e_nb, phi_n).list
+            e = random.choice(list_e_numbers)
+
+            list_k_numbers = KNumbers(start_k_nb, end_k_nb, phi_n, e).list
+
+        k = random.choice(list_k_numbers)
+
+        d = int(((k * phi_n) + 1) / e)
+
+        return e, n, d
+
+    @staticmethod
+    def encrypt(message, e_encryption_public_key, n_encryption_public_key):
+
+        '''
+        :param message: string of characters to encrypt
+        :param e_encryption_public_key: public key that belongs to the recipient to encrypt the message
+        :param n_encryption_public_key: public key that belongs to the recipient to encrypt the message
+        :return: list of encrypted codes
+        '''
+
+        # turn char in ascii code
+        list_ascii_codes = CharToAsciiCode.encrypt(message)
+
+        # process to encrypt with RSA
+        list_encrypted_codes = []
+        for ascii_code in list_ascii_codes:
+            encrypted_code = (ascii_code ** e_encryption_public_key) % n_encryption_public_key
+            list_encrypted_codes.append(encrypted_code)
+
+        return list_encrypted_codes
+
+    @staticmethod
+    def decrypt(list_encrypted_codes, d_decryption_private_key, n_encryption_public_key):
+
+        '''
+        :param list_encrypted_codes: list of encrypted codes to decrypt
+        :param d_decryption_private_key: private key that belongs to the recipient to decrypt the message
+        :param n_encryption_public_key: public key that belongs to the recipient to decrypt the message
+        :return: the decrypted message
+        '''
+
+        # process to decrypt with RSA
+        list_ascii_codes = []
+        for encrypted_code in list_encrypted_codes:
+            ascii_code = (encrypted_code ** d_decryption_private_key) % n_encryption_public_key
+            list_ascii_codes.append(ascii_code)
+
+        # turn ascii codes in char
+        decrypted_message = CharToAsciiCode.decrypt(list_ascii_codes)
+
+        return decrypted_message
+
+
+class ListNumbers:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+
+@singleton
+class PrimeNumbers(ListNumbers):
+
+    def __init__(self, start, end):
+        super().__init__(start, end)
+        self.list = [i for i in range(start, end + 1) if is_prime_number(i)]
+
+
+@singleton
+class ENumbers(ListNumbers):
+
+    def __init__(self, start, end, step, phi_n):
+        super().__init__(start, end)
+        self.step = step
+        self.list = [i for i in range(start, end + 1, step) if not share_common_factor(i, phi_n) and (i % 2 != 0)]
+
+
+class KNumbers(ListNumbers):
+
+    def __init__(self, start, end, phi_n, e):
+        super().__init__(start, end)
+        self.list = [i for i in range(start, end + 1) if ((i * phi_n) + 1) / e == ((i * phi_n) + 1) // e]
